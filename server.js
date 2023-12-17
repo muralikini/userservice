6f421637524bd94e45572fb2e0a1a400b39cf4bc
Muralidhar Kini <muralikini@users.noreply.github.com>
2023-12-17 22:28:02 +0530

    minor change

--- server.js
+++ server.js
@@ -16,6 +16,6 @@ app.use('/api/v1', routes)
 
 const PORT = process.env.PORT || 3003;
 
-app.listen(PORT, () => {
-    console.log(`server is running on PORT ${PORT}`)
+app.listen(3003, () => {
+    console.log(`server is running on PORT ${3003}`)
 })
